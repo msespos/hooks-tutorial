065666872d74f239c4761fe7477c56611583e26b
Mike Esposito <mikeesposito6852@gmail.com>
2023-01-20 16:35:22 -0800

    Use useEffect to update the document title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Example() {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {    
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
   return (
     <div>
       <p>You clicked {count} times</p>
